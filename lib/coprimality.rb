0a08afa46d87da853ced136a89309f4cfde017f0
Art Burtch <artburtch@huskers.unl.edu>
2016-03-14 19:40:53 -0500

    done

--- lib/coprimality.rb
+++ lib/coprimality.rb
@@ -1,32 +1,20 @@
 # This method takes two integers, m and n, and determines whether those integers are co-prime
 
-require 'pry'
 
 def determine_coprimality(m, n)
-  big = 0
-  sml = 0
-  if m > n
-    big = m
-    sml = n
-  elsif n < m
-    big = n
-    sml = m
-  elsif n = m
-    return false
-  end
-  while big != sml
-    dif = big - sml
-    big = dif
-    if big < sml
-      var1 = big
-      big = sml
-      sml = var1
+  big = m
+  sml = n
+  while m != n
+    if m < n
+      var1 = m
+      m = n
+      n = var1
     end
+    dif = m - n
+    m = dif
   end
-  if big > 0
+  if m > 0
     return false
   end
   return true
 end
-
-binding.pry
