eef56acc39c3d205ad9882eef2152225a0971f8d
Art Burtch <artburtch@huskers.unl.edu>
2016-03-14 19:10:04 -0500

    initial commit

--- lib/coprimality.rb
+++ lib/coprimality.rb
@@ -1,6 +1,17 @@
 # This method takes two integers, m and n, and determines whether those integers are co-prime
 
 def determine_coprimality(m, n)
-  #write your method here
+  big = 0
+  sml = 0
+  if m > n
+    big = m
+    sml = n
+  elsif n < m
+    big = n
+    sml = m
+  elsif n = m
+    return false
+  end
+  
   return true
 end
