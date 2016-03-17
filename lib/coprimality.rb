1903713d313196127942f180f37c922f54ac15ec
Art Burtch <artburtch@huskers.unl.edu>
2016-03-17 16:39:22 -0500

    compressed

--- lib/coprimality.rb
+++ lib/coprimality.rb
@@ -1,23 +1,22 @@
-# This method takes two integers, m and n, and determines whether those integers are co-prime
+# Verifies that neither of the numbers are 1, -1, or 0.
+def not_special_case?(arr)
+  !arr.include?(1) && !arr.include?(0)
+end
 
+# This is the repeating subunit for euclid_loop.
+def euclid_singlet(arr)
+  arr.reverse! if arr[0] < arr[1]
+  arr[0] = arr[0] - arr[1]
+end
 
+# Verifies that the GCD of the two numbers is not greater than 1.
+def euclid_loop(arr)
+  euclid_singlet(arr) while arr[1] > 0
+  arr[0] + arr[1] <= 1
+end
+
+# Determines if the given numbers are coprime.
 def determine_coprimality(m, n)
-  if m.abs == 1 || n.abs == 1 || m * n == 0
-    return false
-  end
-  m = m.abs
-  n = n.abs
-  while n > 0
-    if m < n
-      var1 = m
-      m = n
-      n = var1
-    end
-    dif = m - n
-    m = dif
-  end
-  if m + n > 1
-    return false
-  end
-  return true
+  positives = [m.abs, n.abs]
+  not_special_case?(positives) && euclid_loop(positives)
 end
